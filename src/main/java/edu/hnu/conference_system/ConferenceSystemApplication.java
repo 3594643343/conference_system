fb4c1d3ae8d5c631574283354cffb72534050ea8
Wongic <3594643343@qq.com>
2024-11-03 21:25:17 +0800

    创建与pom文件依赖项添加、application.yml文件中增加数据库配置

--- src/main/java/edu/hnu/conference_system/ConferenceSystemApplication.java
+++ src/main/java/edu/hnu/conference_system/ConferenceSystemApplication.java
@@ -2,6 +2,7 @@ package edu.hnu.conference_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ConferenceSystemApplication {

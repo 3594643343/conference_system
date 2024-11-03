24633c51999b56f670ef768774724e12ed134c0a
Wongic <3594643343@qq.com>
2024-11-03 20:53:34 +0800

    创建

---
+++ src/main/java/edu/hnu/conference_system/ConferenceSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.hnu.conference_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSystemApplication.class, args);
+	}
+
+}
